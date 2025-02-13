6bee0c6c4f600e54d66a1b27cfea025eead71e02
Cawan Oliveira <cawanoliveira710@gmail.com>
2025-02-13 14:00:32 -0300

    Create app_styles.dart

---
+++ lib/constants/app_styles.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppStyles {
+  static const TextStyle titleStyle = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle subtitleStyle = TextStyle(
+    fontSize: 16,
+    color: Colors.grey,
+  );
+}
+```
